800d370f773825ef21cd55f5c33000666b52af75
namdeft <dinhnam2306@gmail.com>
2024-01-08 17:32:03 +0700

    add migration db

---
+++ internal/database/migration/000001_init_schema.down.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS dish_ingredient;
+
+DROP TABLE IF EXISTS dish_origin;
+
+DROP TABLE IF EXISTS dish_category;
+
+DROP TABLE IF EXISTS dish;
+
+DROP TABLE IF EXISTS ingredient;
+
+DROP TABLE IF EXISTS origin;
+
+DROP TABLE IF EXISTS category DROP TABLE IF EXISTS user;
